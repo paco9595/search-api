a36b354bd916711290ab64b7000cd11ea26448be
Francisco Villarreal <paco_vip95@hotmail.com>
2019-02-18 02:15:06 -0600

    make call to databse

---
+++ controllers/searchController.js
@@ -0,0 +1,25 @@
+import { connection } from './../services/mysqlService'
+
+export const searchall = (req,res)=>{
+    let resultados = []
+    const { word } = req.params
+    console.log(word)
+    connection.query(`
+        select nombre from empresa 
+        where nombre LIKE '%${word}%'
+    `,(err,results,fields)=>{
+        if (err) console.log(err)
+        resultados = results.map(i=>({nombre:i.nombre, type:'empresa'}))
+        connection.query(`
+            select tag from tag 
+            where tag LIKE '%${word}%'
+        `,(err,results,fields)=>{
+            if (err) console.log(err)
+            const rest = results.map(i=>({nombre: i.tag,type:'tag'}))
+            resultados = [...resultados,...rest]
+            res.status(200).send(resultados)
+        })
+
+    })
+
+}
