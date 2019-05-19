7481fce7c4a90bd39fb2cb9b71c2ccc88fba923a
Francisco Villarreal <francisco.j.padilla@accenture.com>
2019-05-19 17:30:26 -0500

    add getuser info

--- middelware/authVaild.js
+++ middelware/authVaild.js
@@ -9,10 +9,6 @@ export const userAuth = (req, res, next) => {
         }
         var token = req.headers.authorization.replace(/['"]+/g, '');
         var payload = jwt.decode(token, secret)
-        console.log(payload)
-        // User.findById(payload._id, err =>{
-        //     if(err) return res.status(500).send({msg:"error validando el token"})
-        // })
     } catch (ex) {
         if (ex.message === "Token expired") return res.status(401).send({ msg: "token expirado" })
         return res.status(403).send({ msg: "Token no vaildo" })
--- rutas/userRutas.js
+++ rutas/userRutas.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { login, createUser } from './../controllers'
+import { login, createUser, getUser } from './../controllers'
+import { userAuth } from './../middelware/authVaild'
 const userRutas = express.Router();
 
 userRutas.get('/', login)
 userRutas.post('/', createUser)
+userRutas.get('/:id', userAuth, getUser)
 
 // userRutas.put('/', updateEmpresa)
 
